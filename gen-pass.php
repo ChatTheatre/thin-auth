1b9c0eeae9fcc7333fe10380cb90ed979855eb74
Shannon Appelcline <shannon.appelcline@gmail.com>
2021-01-13 08:53:33 -1000

    passwd generator

---
+++ gen-pass.php
@@ -0,0 +1,19 @@
+<?php
+
+(PHP_SAPI !== 'cli' || isset($_SERVER['HTTP_USER_AGENT'])) && die('ERROR: This i
+s a command-line program.');
+
+if (isset($argc) && $argc > 1) {
+  $password=$argv[1];
+
+  echo password_hash($password,PASSWORD_DEFAULT);
+  echo "\n";
+  
+} else {
+
+   echo "ERROR: You must send as a command line argument of the password to conv
+ert.\n";
+
+}
+
+?>
